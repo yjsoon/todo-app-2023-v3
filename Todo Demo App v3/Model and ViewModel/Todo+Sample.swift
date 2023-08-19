845ecf2201c8584c4b01c7b8ecba54e6967a2647
YJ Soon <yjsoon@tinkertanker.com>
2023-08-19 10:55:01 +0800

    Added sample todos

---
+++ Todo Demo App v3/Model and ViewModel/Todo+Sample.swift
@@ -0,0 +1,20 @@
+//
+//  Todo+Sample.swift
+//  Todo Demo App v3
+//
+//  Created by YJ Soon on 19/8/23.
+//
+
+import Foundation
+
+extension Todo {
+    
+    // Todo.sampleTodos
+    static let sampleTodos = [
+        Todo(title: "Get up", isDone: true),
+        Todo(title: "Eat breakfast", subtitle: "McGriddles Stack"),
+        Todo(title: "Brush teeth"),
+        Todo(title: "Go to school")
+    ]
+    
+}
