b0d1546505779f38a819e2077958cd9393de6723
YJ Soon <yjsoon@tinkertanker.com>
2023-08-19 09:09:21 +0800

    Initial Commit

---
+++ Todo Demo App v3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Todo Demo App v3
+//
+//  Created by YJ Soon on 19/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todo Demo App v3/Todo_Demo_App_v3App.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_Demo_App_v3App.swift
+//  Todo Demo App v3
+//
+//  Created by YJ Soon on 19/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_Demo_App_v3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
